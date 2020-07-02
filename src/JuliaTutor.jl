a937975e214a7e35c58e729b6f65d237ae47df75
Casey Kneale <ckneale@udel.com>
2020-07-01 22:53:09 -0400

    init commit

--- src/JuliaTutor.jl
+++ src/JuliaTutor.jl
@@ -1,5 +1,57 @@
 module JuliaTutor
+    using Crayons
 
-# Write your package code here.
+    greet() = print("Hi my name is JuliaTutor, \n
+                    Here you can learn interactively through pre-made lessons.")
 
-end
+    exit_attempt(ui) = lowercase(ui[1:min(4,length(ui))]) == "exit"
+
+    function inform(query)
+        println( Crayon(foreground = :blue, bold = true),
+                 "> ",
+                 Crayon(foreground = :white, bold = false),
+                 query)
+        println( Crayon(foreground = :blue, italics = true),
+                 "...Press any key to continue...")
+        userinput = readline()
+        exit_attempt(userinput) && exit()
+    end
+
+    function evaluate(ui, ans)::Tuple{ Bool, Bool }
+        #try to run the users code
+        syntax_error,incorrect_answer = false, false;
+        try
+            eval(ui)
+        catch
+            syntax_error = true;
+        end
+        incorrect_answer = !(ui == ans)
+        return (syntax_error, incorrect_answer)
+    end
+
+    function request_read_evaluate(request, answer)
+        print( Crayon( foreground = :red, bold = true ),
+                 "> ")
+        println( Crayon( foreground = :white, bold = true ),
+                 request )
+        println(" ")
+        correct = false
+        while !correct
+            print( Crayon( foreground = :green ), "> ")
+            userinput = readline()
+            exit_attempt(userinput) && exit()
+            (syntax_error, incorrect_answer) = evaluate(userinput, answer)
+            if syntax_error
+                println(Crayon( foreground = :white, italics = true ),
+                        "Sorry it appears there is a syntax error in your code.")
+            elseif incorrect_answer
+                println(Crayon( foreground = :white, italics = true ),
+                        "Good try but, this isn't the answer we are looking for. Please try again.")
+            end
+            correct = !syntax_error && !incorrect_answer
+        end
+    end
+
+    export inform, request_read_evaluate, evaluate
+
+end # module
---
+++ src/tst.jl
@@ -0,0 +1,12 @@
+using Pkg
+Pkg.activate("JuliaTutor")
+using JuliaTutor
+
+JuliaTutor.greet()
+
+"""this is some sort of prompt or something.
+maybe some long instruction set!
+maybe someone droning on and on about julia?""" |> inform
+
+request_read_evaluate( "Define a variable `a` to be the integer 1234",
+                    "a=1234")
