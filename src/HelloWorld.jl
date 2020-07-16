06f3954e5eec15e3142794c08603b11cda0f36e3
Casey Kneale <ckneale@udel.com>
2020-07-16 17:28:05 -0400

    some new features

--- src/HelloWorld.jl
+++ src/HelloWorld.jl
@@ -17,5 +17,24 @@ console_output = \"Hello World\"
 Okay now it's your turn,""" |> inform
 
 request_read_evaluate( "Define a string titled \'console_output\' to contain the text \"Hello World\"",
-                    "console_output = \"Hello World\"")
+                    :console_output, "Hello World")
 #console_output = "Hello World"
+
+"""Awesome! So, in a REPL(Read-Evaluate-Print-Loop) you may see the the output of this string
+returned to you after defining a variable. To suppress this, you can add a semilcolon to the end of
+any line of Julia code. This tells the Julia parser to reach the following code as a new instruction,
+or, to suppress outputs (kind of like Matlab).
+console_output = \"Hello World\";
+
+But, now we must learn how to print to console. There are two functions that let you do this:
+print() and println(). Print will display the String or variable as it is, but println will add
+new line/carriage return to the end of it.
+
+Now that our console_output variable is stored in memory, we can print it as follows:
+
+print(console_output)""" |> inform
+
+console_output = "Hello World"
+
+request_read_evaluate(  "Print your \'console_output\' variable to the console",
+                        "Hello World" )
--- src/JuliaTutor.jl
+++ src/JuliaTutor.jl
@@ -1,19 +1,74 @@
 module JuliaTutor
-    using Crayons
-
+    using Crayons, Logging
     greet() = print("Hi my name is JuliaTutor, \n
                     Here you can learn interactively through pre-made lessons.")
+
+    cols = "black, red, green, yellow, blue, magenta, cyan, light_gray, default, dark_gray, light_red, light_green, light_yellow, light_blue, light_magenta, light_cyan, white"
+    fxs = "reset, bold, faint, italics, underline, blink, negative, conceal, strikethrough"
+
+    for (col, fx) in Iterators.product( split(cols, ", "), split(fxs, ", ") )
+        name = Symbol(col * "_" * fx)
+        global colsym = Symbol( col )
+        global fxsym = Symbol( fx )
+        @eval $colsym = colsym
+        @eval $fxsym = fxsym
+        @eval $name() = Crayon( foreground = $colsym, $fxsym = true)
+    end
+
+    #https://github.com/travigd/ComparativeAutograder.jl/blob/f9e99eaec800ecb8ca5f919e820b967dd27fe063/src/stdio.jl#L3
+    #Travis DePrato
+    """
+        @capture_stdstreams(expr)
+    Evaluate the specified expression and return a `(stdout, stderr)` tuple.
+    This macro will attempt to capture every form of output, including via the
+    `Logging` module (_e.g._ the `@logmsg` and friend macros), but it's unable to
+    do this with perfect accuracy. Any code that stores a reference to `stdout` or
+    `stderr` before this is executed will retain those references to the original
+    streams.
+    For example, the original log instance may be obtained using
+    `Logging.global_logger()` because no attempt is made to set the global logger
+    instance.
+    # Examples
+    ```julia-repl
+    julia> out, err = ComparativeAutograder.@capture_stdstreams begin
+               println("I'm going to out!")
+               println(stderr, "I'm going to err!")
+           end
+    ("I'm going to out!\\n", "I'm going to err!\\n")
+    ```
+    """
+    macro capture_stdstreams(expr)
+        # We use a whole bunch of `gensym`'s to avoid polluting the namespace
+        out, outread, outwrite, outtask = gensym.([:stdout, :outread, :outwrite, :outtask])
+        err, errread, errwrite, errtask = gensym.([:stderr, :errread, :errwrite, :errtask])
+        return quote
+            $out, $err = Base.stdout, Base.stderr
+            $outread, $outwrite = redirect_stdout()
+            $errread, $errwrite = redirect_stderr()
+            $outtask = @async read($outread, String)
+            $errtask = @async read($errread, String)
+            try
+                with_logger(ConsoleLogger(Base.stderr, Logging.Debug)) do
+                    $(esc(expr))
+                end
+            catch (e)
+                rethrow(e)
+            finally
+                redirect_stdout($out)
+                redirect_stderr($err)
+                close.([$outwrite, $errwrite])
+            end
+            fetch($outtask), fetch($errtask)
+        end
+    end
 
     exit_attempt(ui) = lowercase(ui[1:min(4,length(ui))]) == "exit"
     strip_whitespace(ui) = filter(x -> !isspace(x), ui)
 
     function inform(query)
-        println( Crayon(foreground = :blue, bold = true),
-                 "> ",
-                 Crayon(foreground = :white, bold = false),
-                 query)
-        println( Crayon(foreground = :blue, italics = true),
-                 "...Press any key to continue...")
+        println( Crayon(foreground = :blue, bold = true), "> ",
+                 Crayon(foreground = :white, bold = false), query)
+        println( Crayon(foreground = :blue, italics = true), "...Press any key to continue...")
         userinput = readline()
         exit_attempt(userinput) && exit()
     end
@@ -21,47 +76,68 @@ module JuliaTutor
     function evaluate(ui, var, ans)::Tuple{ Bool, Bool }
         #try to run the users code
         syntax_error, incorrect_answer = false, false;
-        ui_ex = Meta.parse(ui)
-        try
-            eval(ui_ex)
-        catch
-            syntax_error = true;
-        end
+        ui_ex = Meta.parse( ui )
+        try; eval( ui_ex ); catch; syntax_error = true; end
         # check whether the variable is defined
-        try
-            eval(var)
-        catch
-            incorrect_answer = true;
-        end
+        try; eval( var ); catch; incorrect_answer = true; end
         # if defined check whether it is set to the correct answer
         !incorrect_answer && (incorrect_answer = !(eval(var) === ans))
         return (syntax_error, incorrect_answer)
     end
 
+    function evaluate(ui, truth)::Tuple{ Bool, Bool }
+        syntax_error, incorrect_answer = false, false;
+        ui_ex = Meta.parse( ui )
+        result = nothing
+        try; result = @capture_stdstreams( Main.eval( ui_ex ) ); catch; syntax_error = true; end
+        # Check whether it is set to the correct answer
+        if !isnothing( result)
+            incorrect_answer = !( first( result ) == truth )
+        else
+            incorrect_answer = false
+        end
+        return ( syntax_error, incorrect_answer )
+    end
+
     function request_read_evaluate(request, var, answer)
-        print( Crayon( foreground = :red, bold = true ),
-                 "> ")
-        println( Crayon( foreground = :white, bold = false ),
-                 request )
-        println(" ")
+        print( Crayon( foreground = :red, bold = true ), "> " )
+        println( Crayon( foreground = :white, bold = false ), request, "\n" )
+        correct = false
+        while !correct
+            print( Crayon( foreground = :green, italics = false ), "> " )
+            userinput = readline()
+            #userinput = strip_whitespace(userinput)
+            exit_attempt(userinput) && exit()
+            (syntax_error, incorrect_answer) = evaluate(userinput, var, answer)
+            if syntax_error
+                println(Crayon( foreground = :white, italics = true ), "🔧 Sorry it appears there is a syntax error in your code. Please try again!")
+            elseif incorrect_answer
+                println(Crayon( foreground = :white, italics = true ), "⅁ Good try but, this isn't the answer we are looking for. Please try again.")
+            end
+            correct = !syntax_error && !incorrect_answer
+        end
+        println(Crayon( foreground = :blue, italics = true ), "✔ Good job!" )
+    end
+
+    function request_read_evaluate(request, answer)
+        print( Crayon( foreground = :red, bold = true ), "> ")
+        println( Crayon( foreground = :white, bold = false ), request, "\n")
         correct = false
         while !correct
             print( Crayon( foreground = :green, italics = false ), "> ")
             userinput = readline()
             #userinput = strip_whitespace(userinput)
             exit_attempt(userinput) && exit()
-            (syntax_error, incorrect_answer) = evaluate(userinput, var, answer)
+            (syntax_error, incorrect_answer) = evaluate(userinput, answer)
             if syntax_error
-                println(Crayon( foreground = :white, italics = true ),
-                        "Sorry it appears there is a syntax error in your code.")
+                println(Crayon( foreground = :white, italics = true ), "🔧 Sorry it appears there is a syntax error in your code. Please try again!")
             elseif incorrect_answer
-                println(Crayon( foreground = :white, italics = true ),
-                        "Good try but, this isn't the answer we are looking for. Please try again.")
+                println(Crayon( foreground = :white, italics = true ), "⅁ Good try but, this isn't the answer we are looking for. Please try again.")
             end
             correct = !syntax_error && !incorrect_answer
         end
+        println(Crayon( foreground = :blue, italics = true ), "✔ Good job!" )
     end
-
     export inform, request_read_evaluate, evaluate
 
 end # module
