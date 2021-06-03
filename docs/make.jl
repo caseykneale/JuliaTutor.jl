fc057e0470b05d0f6f96789f8e593c9027f92e85
azzaare <jf@baffier.fr>
2021-06-03 17:04:30 +0900

    Attempt to fix docs generation

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,16 @@
 using JuliaTutor
 using Documenter
 
+@info "Makeing documentation..."
 makedocs(;
     modules=[JuliaTutor],
     authors="caseykneale",
-    repo="https://github.com/caseykneale/JuliaTutor.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/Humans-of-Julia/JuliaTutor.jl/blob/{commit}{path}#L{line}",
     sitename="JuliaTutor.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://caseykneale.github.io/JuliaTutor.jl",
-        assets=String[],
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        canonical="https://Humans-of-Julia.github.io/JuliaTutor.jl",
+        assets=["assets/favicon.ico"; "assets/github_buttons.js"; "assets/custom.css"],
     ),
     pages=[
         "Home" => "index.md",
@@ -17,5 +18,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/caseykneale/JuliaTutor.jl",
+    repo="github.com/Humans-of-Julia/JuliaTutor.jl",
+    devbranch="master",
 )
